814ef96d53d856d12941a68a49e561b2d54e9c9f
Stephanie Sison <sjsison27@icloud.com>
2022-05-28 20:24:36 -0700

    commit tree f1bb567b73e9cae3e92536ac369b7d4373ca104c

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    let results = record.find(record => record.result === 'W');
+    console.log(results);
+    if (results) {
+        return results.year;
+    } else {return undefined
+    }
+}
